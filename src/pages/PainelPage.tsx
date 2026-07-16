79c0a1e9e9322e3919c126db8b65b671904cc0fa
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2026-07-16 13:22:52 -0300

    refactor: remove redundant hideTitle configuration from dashboard UI settings

--- src/pages/PainelPage.tsx
+++ src/pages/PainelPage.tsx
@@ -120,7 +120,6 @@ export const PainelPage = () => {
       fetchGuestToken: () =>
         supersetApi.getGuestToken(painel.embedDashboardUuid!),
       dashboardUiConfig: {
-        hideTitle: false,
         filters: { expanded: true, visible: true },
         hideTab: false,
         hideChartControls: false,
